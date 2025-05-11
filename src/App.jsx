68144a93145b9ae8b00e3a64a9c007089ea6d135
Cain46 <Cain46@gmail.com>
2025-05-11 14:14:38 -0300

    Fase 2 - Configuración de rutas y componentes base

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './modules/landing/Landing';
+import Login from './modules/login/Login';
+import Explorador from './modules/explorador/Explorador';
+import Lector from './modules/lector/Lector';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/explorador" element={<Explorador />} />
+        <Route path="/lector" element={<Lector />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
